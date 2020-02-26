d7cc7772ba486e2e78a18bd450c3748a4314ad28
Marco A. Sueiro Bau <msueiro12@gmail.com>
2020-02-26 20:16:41 +0100

    init

---
+++ precache-manifest.20bcecc761253b40f77aeece48716a1f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d5906ede51525902d43d8fd4ef291fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "453a542ab4c4f312966b",
+    "url": "/static/css/main.93e0a062.chunk.css"
+  },
+  {
+    "revision": "bf4a5c12175e39155913",
+    "url": "/static/js/2.de4db8e8.chunk.js"
+  },
+  {
+    "revision": "453a542ab4c4f312966b",
+    "url": "/static/js/main.ef5f5798.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ac59e568e7c81bb59e2b1372923b6fe3",
+    "url": "/static/media/DotNetPlainWordmark.ac59e568.svg"
+  },
+  {
+    "revision": "e643a6cf77b4635dda856f10fa9b7db4",
+    "url": "/static/media/bg11.e643a6cf.jpg"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "/static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "d7d1aaf71d275992031bd75cdb6ec65c",
+    "url": "/static/media/bg6.d7d1aaf7.jpg"
+  },
+  {
+    "revision": "607a4a36d058e55773625ba23518e291",
+    "url": "/static/media/bg7.607a4a36.jpg"
+  },
+  {
+    "revision": "6737ce3b2577a1c3977e4d479511f642",
+    "url": "/static/media/bg8.6737ce3b.jpg"
+  },
+  {
+    "revision": "4a822c6a36f8f54ff721cef1c06ec718",
+    "url": "/static/media/marco.4a822c6a.png"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
